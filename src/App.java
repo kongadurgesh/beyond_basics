d7f9ade4e01556aea1ecfb600a0ac0611b766249
Durgesh Konga <kongadurgesh20@gmail.com>
2025-04-19 22:36:32 +0530

    3rd commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int test = 1;
+        switch (test) {
+            case 1:
+
+                break;
+
+            default:
+                break;
+        }
     }
 }
